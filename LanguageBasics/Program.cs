15df9262dae2314a28db84b26e10b17291b1a2b6
Henrik Bulskov <bulskov@ruc.dk>
2020-10-13 11:10:19 +0200

    Add project files.

---
+++ LanguageBasics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LanguageBasics
+{
+    class Util
+    {
+        public static int a = 5;
+    }
+    class Program
+    {
+
+        static void Swap(ref int x, ref int y)
+        {
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+    
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var a = 5;
+            var b = 6;
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a} and b = {b}");
+
+        }
+    }
+}
